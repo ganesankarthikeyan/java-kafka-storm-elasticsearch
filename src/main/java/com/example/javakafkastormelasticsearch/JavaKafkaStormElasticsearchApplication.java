3b67ae5c73a7330f7cc4d3a85c553d359e4c2dee
ganesankarthikeyan <karthik.ganesankarthik.ganesan@gmail.com>
2020-06-03 23:25:29 +0530

    created structure

---
+++ src/main/java/com/example/javakafkastormelasticsearch/JavaKafkaStormElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.javakafkastormelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKafkaStormElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKafkaStormElasticsearchApplication.class, args);
+	}
+
+}
